a8a9d3f5cbd97737495064e9e683325279064559
Braxton Fair <hello@taxborn.com>
2022-03-06 19:53:46 -0600

    Lexer

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,24 @@ struct Args {
     count: usize,
 }
 
+#[derive(Debug, Clone)]
+enum Token {
+    IncrementX,
+    DecrementX,
+    IncrementY,
+    DecrementY,
+    IncrementZ,
+    DecrementZ,
+    IncrementW,
+    DecrementW,
+    Increment,
+    Decrement,
+    Write,
+    Read,
+    LoopBegin,
+    LoopEnd
+}
+
 fn main() {
     let args = Args::parse();
     let mut file = File::open(args.file).expect("Open failed.");
@@ -30,4 +48,39 @@ fn main() {
 
     let mut mem = Array::<u32, Ix4>
         ::zeros((args.count, args.count, args.count, args.count));
+
+    let tokens = lexer(source);
+
+    println!("{:?}", tokens);
+}
+
+fn lexer(source: String) -> Vec<Token> {
+    let mut tokens = Vec::new();
+
+    for symbol in source.chars() {
+        let token = match symbol {
+            '+' => Some(Token::Increment),
+            '-' => Some(Token::Decrement),
+            '>' => Some(Token::IncrementX),
+            '<' => Some(Token::DecrementX),
+            '^' => Some(Token::IncrementY),
+            'V' | 'v' => Some(Token::DecrementY),
+            '*' => Some(Token::IncrementZ),
+            'O' | 'o' => Some(Token::DecrementZ),
+            '@' => Some(Token::IncrementW),
+            '?' => Some(Token::DecrementW),
+            '.' => Some(Token::Write),
+            ',' => Some(Token::Read),
+            '[' => Some(Token::LoopBegin),
+            ']' => Some(Token::LoopEnd),
+            _   => None
+        };
+
+        match token {
+            Some(token) => tokens.push(token),
+            None      => (),
+        };
+    }
+
+    tokens
 }
