4f084e62b3521366071b0c21081b38ee7aefa836
Braxton Fair <hello@taxborn.com>
2022-03-08 11:12:00 -0600

    Start error handling work

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #![allow(non_snake_case)]
+use std::fmt;
 use std::fs::File;
 use std::io::Read;
 
+// Libraries used
 use clap::Parser;
 use ndarray::{Array, Ix4};
 
+// A custom type for readability for a cell.
 type Cell = usize;
 
 /// Argument struct for the CLI
@@ -14,13 +17,17 @@ struct Args {
     /// The file name
     #[clap(short, long)]
     file: String,
-
-    /// The side length of the hypercube. This shouldn't change to adhere to
-    /// the spec, but might be interesting to play around with.
-    #[clap(short, long, default_value_t = 8)]
-    count: Cell,
+    // The side length of the hypercube. This shouldn't change to adhere to
+    // the spec, but might be interesting to play around with.
+    //
+    // TODO: The barrier here is that this is a runtime found value, and that
+    // causes issues if I want to use the value at compile time.
+    //#[clap(short, long, default_value_t = 8)]
+    //count: Cell,
 }
 
+// The location struct to hold the current position of our program
+#[derive(Clone, Copy)]
 struct Loc {
     x: Cell,
     y: Cell,
@@ -28,12 +35,68 @@ struct Loc {
     w: Cell,
 }
 
-impl Loc {
-    fn new() -> Self {
-        Loc { x: 0, y: 0, z: 0, w: 0 }
+#[derive(Debug)]
+enum Direction {
+    XPos,
+    XNeg,
+    YPos,
+    YNeg,
+    ZPos,
+    ZNeg,
+    WPos,
+    WNeg,
+}
+
+// Use a unit struct to declare custom 'Falling off the hypercube' error.
+struct MovError {
+    direction: Direction,
+}
+
+// Implementing std::fmt::Display for MovError
+impl fmt::Display for MovError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "ERROR: Fell off the hypercube, in the {:?} direction",
+            self.direction
+        )
     }
 }
 
+impl Loc {
+    // Create a new location struct
+    fn new() -> Self {
+        Loc {
+            x: 0,
+            y: 0,
+            z: 0,
+            w: 0,
+        }
+    }
+
+    fn mov(
+        mut self,
+        direction: Direction,
+        steps: isize,
+    ) -> Result<(), MovError> {
+        match direction {
+            Direction::XPos => {
+                println!("before: moved {} steps. curr: {}", steps, self.x);
+                if self.x + steps as usize >= 8 {
+                    return Err(MovError { direction: Direction::XPos });
+                }
+
+                self.x += steps as usize;
+                println!("after: moved {} steps. curr: {}", steps, self.x);
+            },
+            _ => ()
+        }
+
+        Ok(())
+    }
+}
+
+// All the tokens in our program
 #[derive(Debug, Clone)]
 enum Token {
     IncrementX,
@@ -74,16 +137,20 @@ fn main() {
     let mut file = File::open(args.file).expect("Open failed.");
     let mut source = String::new();
 
-    file.read_to_string(&mut source).expect("Read to string failed.");
+    file.read_to_string(&mut source)
+        .expect("Read to string failed.");
 
-    let mut mem = Array::<u8, Ix4>
-        ::zeros((args.count, args.count, args.count, args.count));
+    let mut mem = Array::<u8, Ix4>::zeros((8, 8, 8, 8));
 
     let tokens = lexer(source);
     let instructions = parser(tokens);
 
     let mut locator = Loc::new();
-    run(instructions, &mut mem, &mut locator);
+
+    match run(instructions, &mut mem, &mut locator) {
+        Ok(()) => (),
+        Err(e) => eprintln!("{}", e),
+    }
 }
 
 fn lexer(source: String) -> Vec<Token> {
@@ -105,10 +172,11 @@ fn lexer(source: String) -> Vec<Token> {
             ',' => Some(Token::Read),
             '[' => Some(Token::LoopBegin),
             ']' => Some(Token::LoopEnd),
-            _   => None
-        }; match token {
+            _ => None,
+        };
+        match token {
             Some(token) => tokens.push(token),
-            None        => (),
+            None => (),
         };
     }
 
@@ -141,8 +209,9 @@ fn parser(tokens: Vec<Token>) -> Vec<Instruction> {
 
                     None
                 }
-                Token::LoopEnd => 
-                    panic!("Loop ending at #{} has no beginning.", idx),
+                Token::LoopEnd => {
+                    panic!("Loop ending at #{} has no beginning.", idx)
+                }
             };
 
             match instruction {
@@ -177,24 +246,59 @@ fn parser(tokens: Vec<Token>) -> Vec<Instruction> {
     instructions
 }
 
-fn run(instructions: Vec<Instruction>, mem: &mut Array::<u8, Ix4>, 
-        locator: &mut Loc) {
+fn run(
+    instructions: Vec<Instruction>,
+    mem: &mut Array<u8, Ix4>,
+    locator: &mut Loc,
+) -> Result<(), MovError> {
     for instruction in instructions {
         match instruction {
-            Instruction::IncrementX => locator.x += 1,
-            Instruction::DecrementX => locator.x -= 1,
-            Instruction::IncrementY => locator.y += 1,
-            Instruction::DecrementY => locator.y -= 1,
-            Instruction::IncrementZ => locator.z += 1,
-            Instruction::DecrementZ => locator.z -= 1,
-            Instruction::IncrementW => locator.w += 1,
-            Instruction::DecrementW => locator.w -= 1,
+            Instruction::IncrementX => {
+                if let Err(e) = locator.mov(Direction::XPos, 1) {
+                    return Err(e);
+                }
+            }
+            Instruction::DecrementX => {
+                if let Err(e) = locator.mov(Direction::XNeg, 1) {
+                    return Err(e);
+                }
+            }
+            Instruction::IncrementY => {
+                if let Err(e) = locator.mov(Direction::YPos, 1) {
+                    return Err(e);
+                }
+            }
+            Instruction::DecrementY => {
+                if let Err(e) = locator.mov(Direction::YNeg, 1) {
+                    return Err(e);
+                }
+            }
+            Instruction::IncrementZ => {
+                if let Err(e) = locator.mov(Direction::ZPos, 1) {
+                    return Err(e);
+                }
+            }
+            Instruction::DecrementZ => {
+                if let Err(e) = locator.mov(Direction::ZNeg, 1) {
+                    return Err(e);
+                }
+            }
+            Instruction::IncrementW => {
+                if let Err(e) = locator.mov(Direction::WPos, 1) {
+                    return Err(e);
+                }
+            }
+            Instruction::DecrementW => {
+                if let Err(e) = locator.mov(Direction::WNeg, 1) {
+                    return Err(e);
+                }
+            }
             Instruction::Increment => {
                 mem[[locator.x, locator.y, locator.z, locator.w]] += 1
-            },
+            }
             Instruction::Decrement => {
                 mem[[locator.x, locator.y, locator.z, locator.w]] -= 1
-            },
+            }
             Instruction::Write => print!(
                 "{}",
                 mem[[locator.x, locator.y, locator.z, locator.w]] as char
@@ -209,13 +313,17 @@ fn run(instructions: Vec<Instruction>, mem: &mut Array::<u8, Ix4>,
                 mem[[locator.x, locator.y, locator.z, locator.w]] = input[0];
             }
             Instruction::Loop(instructions) => {
-                let zeroArray = Array::<u8, Ix4>
-                    ::zeros((8, 8, 8, 8));
+                let zeroArray = Array::<u8, Ix4>::zeros((8, 8, 8, 8));
 
-                while mem != &zeroArray {
-                    run(instructions.to_owned(), mem, locator);
+                while mem.to_owned() != zeroArray {
+                    match run(instructions.to_owned(), mem, locator) {
+                        Ok(()) => (),
+                        Err(e) => eprintln!("{}", e),
+                    }
                 }
             }
         }
     }
+
+    Ok(())
 }
