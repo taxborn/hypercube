4569b03ba40e023fe39c7cacc32a3eea65e58962
Braxton Fair <hello@taxborn.com>
2022-03-10 21:25:33 -0600

    Add macro to reduce repetitiveness

--- src/locator.rs
+++ src/locator.rs
@@ -23,9 +23,31 @@ pub enum Direction {
     WNeg,
 }
 
+// A macro for the repetitive 'directions' match statement.
+macro_rules! direction_pos {
+    ($self:ident, $dir:expr, $var:ident, $steps:expr) => {{
+        if $self.$var + $steps as usize >= $self.count {
+            return Err(MovError { direction: $dir });
+        }
+
+        $self.$var += $steps as usize;
+    }};
+}
+
+macro_rules! direction_neg {
+    ($self:ident, $dir:expr, $var:ident, $steps:expr) => {{
+        if $self.$var as isize - $steps < 0 {
+            return Err(MovError { direction: $dir });
+        }
+
+        $self.$var -= $steps as usize;
+    }};
+}
+
 // A location structure so we can keep track of where we are in memory. This is
 // how we keep track while we are interpreting the program.
 impl Loc {
+    // Create a new Loc structure
     pub fn new(count: usize) -> Self {
         Loc {
             x: 0,
@@ -37,9 +59,9 @@ impl Loc {
     }
 
     // Move a certain direction, a certain number of steps. We prefer using this
-    // function over just manually changing the variables of the structure because
-    // we need to make sure we are in the bounds of the memory. This also gives
-    // us a good way to describe movement.
+    // function over just manually changing the variables of the structure
+    // because we need to make sure we are in the bounds of the memory.
+    // This also gives us a good way to describe movement.
     pub fn mov(
         &mut self,
         direction: Direction,
@@ -47,76 +69,28 @@ impl Loc {
     ) -> Result<(), MovError> {
         match direction {
             Direction::XPos => {
-                if self.x + steps as usize >= self.count {
-                    return Err(MovError {
-                        direction: Direction::XPos,
-                    });
-                }
-
-                self.x += steps as usize;
+                direction_pos!(self, Direction::XPos, x, steps)
             }
             Direction::XNeg => {
-                if self.x as isize - steps < 0 {
-                    return Err(MovError {
-                        direction: Direction::XNeg,
-                    });
-                }
-
-                self.x -= steps as usize;
+                direction_neg!(self, Direction::XNeg, x, steps)
             }
             Direction::YPos => {
-                if self.y + steps as usize >= self.count {
-                    return Err(MovError {
-                        direction: Direction::YPos,
-                    });
-                }
-
-                self.y += steps as usize;
+                direction_pos!(self, Direction::YPos, y, steps)
             }
             Direction::YNeg => {
-                if self.y as isize - steps < 0 {
-                    return Err(MovError {
-                        direction: Direction::YNeg,
-                    });
-                }
-
-                self.y -= steps as usize;
+                direction_neg!(self, Direction::YNeg, y, steps)
             }
             Direction::ZPos => {
-                if self.z + steps as usize >= self.count {
-                    return Err(MovError {
-                        direction: Direction::ZPos,
-                    });
-                }
-
-                self.z += steps as usize;
+                direction_pos!(self, Direction::ZPos, z, steps)
             }
             Direction::ZNeg => {
-                if self.z as isize - steps < 0 {
-                    return Err(MovError {
-                        direction: Direction::ZNeg,
-                    });
-                }
-
-                self.z -= steps as usize;
+                direction_neg!(self, Direction::ZNeg, z, steps)
             }
             Direction::WPos => {
-                if self.w + steps as usize >= self.count {
-                    return Err(MovError {
-                        direction: Direction::WPos,
-                    });
-                }
-
-                self.w += steps as usize;
+                direction_pos!(self, Direction::WPos, w, steps)
             }
             Direction::WNeg => {
-                if self.w as isize - steps < 0 {
-                    return Err(MovError {
-                        direction: Direction::WNeg,
-                    });
-                }
-
-                self.w -= steps as usize;
+                direction_neg!(self, Direction::WNeg, w, steps)
             }
         }
 
